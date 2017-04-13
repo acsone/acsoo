77d9c36eff3e5584869f413fbb4b9d72e12d23f8
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-04-13 14:57:52 +0200

    setuptools<31 until assorted bugs are resolved

--- acsoo/templates/project/+project.name+/bootstrap.sh
+++ acsoo/templates/project/+project.name+/bootstrap.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 pip install -U "pip>=9.0.1"
 pip install -U git+https://github.com/acsone/acsoo.git
+
+# https://github.com/odoo/odoo/pull/15718
+# https://github.com/pypa/pip/issues/4176
+pip install -U "setuptools<31"
