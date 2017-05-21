d2845472e86b01fdda7215b509ab1df9bea62885
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-05-21 12:25:17 +0200

    initialize test suite, first test for pylint

--- acsoo/__init__.py
+++ acsoo/__init__.py
@@ -2,6 +2,8 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
+# flake8: noqa
+
 from . import main
 # commands
 from . import flake8cmd
---
+++ tests/data/odoo/addons/addon1/__manifest__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+{
+    'author': 'ACSONE SA/NV',
+    # TODO ...
+}
