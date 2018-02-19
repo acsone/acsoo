94e7452e1a630e0e9fb61d45846a41d2f971ded6
Benjamin Willig <benjamin.willig@acsone.eu>
2018-02-19 09:28:28 +0100

    Remove __init__files from odoo and odoo/addons

--- acsoo/templates/hooks.py
+++ acsoo/templates/hooks.py
@@ -6,7 +6,14 @@ def post_render_project(configurator):
     variables = configurator.variables
     root = configurator.target_directory
     root = os.path.join(root, variables['project.name'])
-    if variables['odoo.series'] in ('8.0', '9.0'):
+    odoo_serie = variables['odoo.series']
+    if odoo_serie in ('8.0', '9.0'):
         shutil.rmtree(os.path.join(root, 'odoo'))
     else:
         shutil.rmtree(os.path.join(root, 'odoo_addons'))
+
+    if odoo_serie in ('11.0'):
+        # Delete init file
+        filename = '__init__.py'
+        os.remove(os.path.join(root, 'odoo', filename))
+        os.remove(os.path.join(root, 'odoo', 'addons', filename))
