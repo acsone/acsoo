d454cdf2e509a1d156e4979a35a677d871421a89
Benjamin Willig <benjamin.willig@acsone.eu>
2018-02-20 11:29:30 +0100

    [FIX] better handling for python version and related libs

--- acsoo/templates/hooks.py
+++ acsoo/templates/hooks.py
@@ -1,34 +1,30 @@
 import os
 import shutil
-from ..tools import check_output
 
 
 def pre_render_project(configurator):
     variables = configurator.variables
-    odoo_serie = variables['odoo.series']
+    odoo_series = variables['odoo.series']
 
-    if odoo_serie in ('11.0',):
+    if odoo_series in ('11.0',):
         python_version = 'python3'
     else:
         python_version = 'python'
 
     configurator.variables['python_version'] = python_version
-    configurator.variables['python_path'] = check_output([
-        'which', python_version,
-    ])
 
 
 def post_render_project(configurator):
     variables = configurator.variables
     root = configurator.target_directory
     root = os.path.join(root, variables['project.name'])
-    odoo_serie = variables['odoo.series']
-    if odoo_serie in ('8.0', '9.0'):
+    odoo_series = variables['odoo.series']
+    if odoo_series in ('8.0', '9.0'):
         shutil.rmtree(os.path.join(root, 'odoo'))
     else:
         shutil.rmtree(os.path.join(root, 'odoo_addons'))
 
-    if odoo_serie in ('11.0'):
+    if odoo_series in ('11.0'):
         # Delete init file
         filename = '__init__.py'
         os.remove(os.path.join(root, 'odoo', filename))
