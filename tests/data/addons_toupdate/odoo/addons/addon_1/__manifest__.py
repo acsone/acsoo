cfdb4d310cd04ce0ea8098586007f7d561a1c0aa
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2018-02-26 13:56:04 +0100

    [ADD] Addons Toupdate Unit Tests: test data initial state This commits brings the initial state of the data directory used in Acsoo Addons Toupdate unit tests Following commits in this directory (acsoo/tests/data/addons_toupdate) will be used to test the command acsoo addons toupdate

---
+++ tests/data/addons_toupdate/odoo/addons/addon_1/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Addon 1',
+    'description': """
+        Test addon 1 used in acsoo addons toupdate unit tests.""",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV',
+    'website': 'https://acsone.eu/',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
