b41264cbca3ed2b73dc32caa79ad117df574a61d
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2020-02-01 11:48:22 +0100

    update copyright header

--- acsoo/tag_requirements.py
+++ acsoo/tag_requirements.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016-2020 ACSONE SA/NV (<http://acsone.eu>)
 # License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
 import logging
