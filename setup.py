3b2a975c19e8f9179208bf0cd6438b7a2d50bc63
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-05-22 11:23:24 +0200

    Test with python 3.7 and 3.8

--- setup.py
+++ setup.py
@@ -34,8 +34,11 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Framework :: Odoo",
     ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     entry_points="""
         [console_scripts]
         acsoo=acsoo.main:main
