166ed7509db2444a000856ae4a4ebdb424f2fa57
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-05-25 09:40:03 +0200

    [MNT] declarative setuptools configuration

--- setup.py
+++ setup.py
@@ -2,46 +2,9 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License GPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
-    name="acsoo",
-    description="Acsone Odoo Dev Tools",
-    long_description="\n".join((open("README.rst").read(), open("CHANGES.rst").read())),
-    use_scm_version=True,
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        "appdirs",
-        "bobtemplates.odoo",
-        "click",
-        "colorama",
-        "configparser",
-        "flake8",
-        "lxml",  # pylint-odoo dep not installed automatically?
-        "pip>=9.0.1",
-        "pylint-odoo",
-        "setuptools>=20,<31",
-        "wheel>=0.29",
-        "bumpversion",
-    ],
-    license="GPLv3+",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: " "GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: POSIX",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Framework :: Odoo",
-    ],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
-    entry_points="""
-        [console_scripts]
-        acsoo=acsoo.main:main
-    """,
-    options={"bdist_wheel": {"universal": "1"}},
+    # this is still required for python setup.py sdist
+    setup_requires=["setuptools_scm"],
 )
