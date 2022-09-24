506dbbb0ae3f1be0cd96447c16d7a8adfbb2d96e
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-09-24 18:46:27 +0200

    project template: python_version selection

--- acsoo/templates/hooks.py
+++ acsoo/templates/hooks.py
@@ -2,7 +2,6 @@ def pre_render_project(configurator):
     variables = configurator.variables
     odoo_series = variables["odoo.series"]
 
-    configurator.variables["python_version"] = "python3"
     configurator.variables["odoo.major"] = int(odoo_series.split(".")[0])
 
 
